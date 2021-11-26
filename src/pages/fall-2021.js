acf99ec2c04ca0a40c3bca11c55380dae4d7643f
Angelo Fausti <angelofausti@gmail.com>
2021-11-26 06:18:47 -0700

    Initial version

---
+++ src/pages/fall-2021.js
@@ -0,0 +1,18 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+const SecondPage = () => (
+  <Layout>
+    <h3>Fall 2021</h3>
+
+    <p>During this season as soon as our plants started to grow, we noticed Gopher activity starting early November mainly at the Jojobas and Jaguars neighborhoods, on plots 16, 20, 21, 29 and 45.</p>
+
+    <p>We are testing a sonic repeller that vibrates at 400Hz for 3 seconds every 20 seconds. Two sonic repllers were installed at the Jojobas neighborhood on November 23 inside plots 21 and 24.</p>
+
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default SecondPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,23 +8,78 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h3>Monitoring gopher activity at Las Milpitas community farm</h3>
+
+    <p>You can join us by observing and <a href="https://forms.gle/3cVEw7Dy2jsCbkiV8">collecting data about the plots using this form</a>. You can do that at any time and for as many plots as you like. The more data we collect the better!</p>
+    <h3>A note about traps</h3>
+
+    <p>Traps are very effective in killing gophers, but they are not effective in preventing gophers from making damage to our gardens. More gophers will come, and when you feel that you need to install a trap it is probably too late!</p>
+
+    <p>The underground network project aims to find preventive solutions that are more humane and sustainable.</p>
+
+    <p>By collecting data throughout the seasons our goal is to test the effectiveness of these solutions.</p>
+
+
+    <h3>Gopher activity map</h3>
+
+    <p><a href="https://theundergroundnetwork.netlify.app">Go to map</a></p>
+
+    <h3>Results</h3>
+
+    <ul>
+      <li>
+        <p><Link to="/fall-2021/">Fall 2021</Link></p>
+      </li>
+      <li>
+        <p>Spring 2022</p>
+      </li>
+    </ul>
+    <h3>Gallery</h3>
     <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
+      src="../images/gopher2.jpg"
+      height={200}
       quality={95}
       formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
+      alt="Gopher (profile)"
       style={{ marginBottom: `1.45rem` }}
     />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+
+    <StaticImage
+      src="../images/gopher.jpg"
+      height={200}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="Gopher (front)"
+      style={{ marginBottom: `1.45rem` }}
+    />
+
+    <StaticImage
+      src="../images/hole.jpg"
+      height={200}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="Gopher hole"
+      style={{ marginBottom: `1.45rem` }}
+    />
+
+    <StaticImage
+      src="../images/red_shard.jpg"
+      height={200}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="Red Shard"
+      style={{ marginBottom: `1.45rem` }}
+    />
+
+    <StaticImage
+      src="../images/bok_choy.jpg"
+      height={200}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="Bok Choy"
+      style={{ marginBottom: `1.45rem` }}
+    />
+
   </Layout>
 )
 
