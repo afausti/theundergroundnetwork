c9dcb50af11222a8502817e7b8f562a2e8567c4c
Angelo Fausti <angelofausti@gmail.com>
2021-11-29 16:46:47 -0700

    Update results page

--- src/pages/fall-2021.js
+++ src/pages/fall-2021.js
@@ -13,15 +13,14 @@ const SecondPage = () => (
 
     <p>On November 24 gopher activity was reported on a new plot on Jojobas (plot 28), Sandias (plots 56 and 59), Cukes (plots 70, 71, 72) and on The OGs (plot 86).</p>
 
-    <p>We are testing a sonic repeller that vibrates at 400Hz for 3 seconds every 20 seconds. Two sonic repllers were initially installed at the Jojobas neighborhood on November 23 on plots 21 and 24.</p>
-
+    <p>We are testing a <a href="https://www.amazon.com/gp/product/B08KGZSJQW/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1">solar sonic repeller</a> that vibrates at 400Hz for 3 seconds every 20 seconds. Two of these repllers were initially installed at the Jojobas neighborhood on November 23 on plots 21 and 24.</p>
 
     <h3>Gopher activity</h3>
 
-    Gopher activity at the farm as of November 24.
+    Gopher activity and location of the sonic repellers as of November 24.
 
     <StaticImage
-      src="../images/gopher-nov24.png"
+      src="../images/map-nov24.png"
       width={800}
       quality={95}
       formats={["auto", "webp", "avif"]}
