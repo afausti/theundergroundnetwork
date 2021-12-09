f2c97c02a6ce676381ad83cbc877f6e9719f64f7
Angelo Fausti <angelofausti@gmail.com>
2021-12-08 21:00:07 -0700

    Add results from Dec 04

--- src/pages/fall-2021.js
+++ src/pages/fall-2021.js
@@ -9,18 +9,33 @@ const SecondPage = () => (
   <Layout>
     <h3>Fall 2021</h3>
 
-    <p>During this season as soon as our plants started to grow, we noticed Gopher activity on November 20 at the Jojobas and Jaguars neighborhoods, on plots 16, 20, 21, 29 and 45.</p>
+    <p>During this season, as soon as our plants started to grow, we noticed Gopher activity early November at Jojobas (plots 16, 20, 21, and 29) and at Jaguars (plot 45) neighborhoods.</p>
 
-    <p>On November 24 gopher activity was reported on a new plot on Jojobas (plot 28), Sandias (plots 56 and 59), Cukes (plots 70, 71, 72) and on The OGs (plot 86).</p>
+    <p>We are testing a <a href="https://www.amazon.com/gp/product/B08KGZSJQW/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1">sonic repeller</a> initially installed at Jojobas (plots 21 and 24) on November 23.</p>
 
-    <p>We are testing a <a href="https://www.amazon.com/gp/product/B08KGZSJQW/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1">solar sonic repeller</a> that vibrates at 400Hz for 3 seconds every 20 seconds. Two of these repllers were initially installed at the Jojobas neighborhood on November 23 on plots 21 and 24.</p>
+    <p>On November 24 we registered gopher activity on new plots: Jojobas (plot 28), Sandias (plots 56 and 59), Cukes (plots 70, 71, 72) and on The OGs (plot 86) a total of 12 plots.</p>
+
+    <p>On December 04 we registered gophers activity on the Comunity plots, Jojobas (with less activity this time) and increased activity on Sandias (plots 52, 54, 56, and 59) and Cukes (plots 67, 70, 71, 72, and 73) a total of 14 plots. We installed a third sonic repeller and decided to place them on the Community plots, Jojobas and Cukes.</p>
+
+    <p>Chard, Bok Choy, Kale, Beet, and Broccoli are the plants preferred by the gophers.</p>
+
+    <p>Based on the holes fond outside the plots, we think that gophers live under the trees during the summer, where they have their tunnel systems for shelter, and they build new systems underground during the winter to collect food from the gardens.</p>
 
     <h3>Gopher activity</h3>
 
     Gopher activity and location of the sonic repellers as of November 24.
-
     <StaticImage
       src="../images/map-nov24.png"
+      width={800}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="Gopher activity"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <br/>
+    Gopher activity and location of the sonic repellers as of December 04.
+    <StaticImage
+      src="../images/map-dec04.png"
       width={800}
       quality={95}
       formats={["auto", "webp", "avif"]}
